6bd266b19b052090a1153d56282f4837dfd36cbb
Nessa <na.nadine.001@gmail.com>
2024-04-04 11:26:19 +0700

    delete unused code

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -41,7 +41,8 @@ class ContentController extends Controller
         return response()->json('greeting content added succesfully');
     }
 
-    public function greeting_content($hotel_id) {
+    public function greeting_content($hotel_id)
+    {
         $hotel = Hotel::where('id', intval($hotel_id))->first();
 
         try {
@@ -122,7 +123,8 @@ class ContentController extends Controller
         return response()->json('hotel info added succesfully');
     }
 
-    public function hotel_photo(Request $request, $hotel_id) {
+    public function hotel_photo(Request $request, $hotel_id)
+    {
         $validator = Validator::make($request->all(), [
             'hotel_photo' => 'required',
         ]);
@@ -152,7 +154,8 @@ class ContentController extends Controller
         return response()->json('hotel photo saved succesfully');
     }
 
-    public function hotel_qr_code_wifi(Request $request, $hotel_id) {
+    public function hotel_qr_code_wifi(Request $request, $hotel_id)
+    {
         $validator = Validator::make($request->all(), [
             'hotel_qr_code_wifi' => 'required',
         ]);
@@ -182,7 +185,8 @@ class ContentController extends Controller
         return response()->json('hotel QR code wifi saved succesfully');
     }
 
-    public function hotel_qr_code_payment(Request $request, $hotel_id) {
+    public function hotel_qr_code_payment(Request $request, $hotel_id)
+    {
         $validator = Validator::make($request->all(), [
             'hotel_qr_code_payment' => 'required',
         ]);
@@ -246,7 +250,8 @@ class ContentController extends Controller
         return response()->json('hotel facilities added succesfully');
     }
 
-    public function hotel_facilities_data(Request $request, $facility_id) {
+    public function hotel_facilities_data(Request $request, $facility_id)
+    {
         $facility = HotelFacilities::where('id', $facility_id)->first();
 
         try {
@@ -297,7 +302,8 @@ class ContentController extends Controller
         return response()->json('hotel facilities updated succesfully');
     }
 
-    public function update_facility_image(Request $request, $facility_id) {
+    public function update_facility_image(Request $request, $facility_id)
+    {
         $validator = Validator::make($request->all(), [
             'image' => 'required',
         ]);
@@ -461,7 +467,8 @@ class ContentController extends Controller
         return response()->json('room about updated succesfully');
     }
 
-    public function update_room_image(Request $request, $room_id) {
+    public function update_room_image(Request $request, $room_id)
+    {
         $validator = Validator::make($request->all(), [
             'image' => 'required',
         ]);
@@ -539,7 +546,8 @@ class ContentController extends Controller
         return response()->json('amenities added succesfully');
     }
 
-    public function amenities_data($service_id) {
+    public function amenities_data($service_id)
+    {
         $service = RoomService::where('id', intval($service_id))->first();
 
         try {
@@ -586,7 +594,8 @@ class ContentController extends Controller
         return response()->json('amenities updated succesfully');
     }
 
-    public function update_amenity_image(Request $request, $service_id) {
+    public function update_amenity_image(Request $request, $service_id)
+    {
         $validator = Validator::make($request->all(), [
             'image' => 'required',
         ]);
@@ -658,7 +667,8 @@ class ContentController extends Controller
         return response()->json('ads lips menu added succesfully');
     }
 
-    public function ads_lips_content($hotel_id) {
+    public function ads_lips_content($hotel_id)
+    {
         try {
             $hotel = Hotel::where('id', $hotel_id)->first();
 
@@ -761,7 +771,8 @@ class ContentController extends Controller
         return response()->json('menu type updated succesfully');
     }
 
-    public function update_menu_type_image(Request $request, $menu_type_id) {
+    public function update_menu_type_image(Request $request, $menu_type_id)
+    {
         $validator = Validator::make($request->all(), [
             'image' => 'required',
         ]);
@@ -845,7 +856,8 @@ class ContentController extends Controller
         return response()->json('menu added succesfully');
     }
 
-    public function menu_data($menu_id) {
+    public function menu_data($menu_id)
+    {
         try {
             $menu = Menu::where('id', intval($menu_id))->first();
 
@@ -901,7 +913,8 @@ class ContentController extends Controller
         return response()->json('menu updated succesfully');
     }
 
-    public function update_menu_image(Request $request, $menu_id) {
+    public function update_menu_image(Request $request, $menu_id)
+    {
         $validator = Validator::make($request->all(), [
             'image' => 'required',
         ]);
@@ -946,143 +959,4 @@ class ContentController extends Controller
             "message" => "menu deleted successfully"
         ]);
     }
-
-    // public function add_content(Request $request)
-    // {
-    //     $validator = Validator::make($request->all(), [
-    //         'greeting' => 'required',
-    //         'hotel_name' => 'required',
-    //         'hotel_class' => 'required',
-    //         // 'hotel_logo' => 'required',
-    //         'hotel_about' => 'required',
-    //         'hotel_check_in' => 'required',
-    //         'hotel_check_out' => 'required',
-    //         'hotel_photo' => 'required',
-    //         'hotel_address' => 'required',
-    //         'hotel_city' => 'required',
-    //         'hotel_phone' => 'required',
-    //         'facility_name' => 'required',
-    //         'facility_image' => 'required',
-    //         'facility_description' => 'required',
-    //         'room_type' => 'required',
-    //         'room_facility' => 'required',
-    //         'room_description' => 'required',
-    //         'room_image' => 'required',
-    //         'room_television' => 'required',
-    //         'room_service_name' => 'required',
-    //         'room_service_image' => 'required',
-    //         'menu_name' => 'required',
-    //         'menu_description' => 'required',
-    //         'menu_price' => 'required',
-    //         'menu_rating' => 'required',
-    //         'menu_image' => 'required',
-    //     ]);
-
-    //     if ($validator->fails()) {
-    //         return response()->json($validator->errors());
-    //     }
-
-    //     DB::beginTransaction();
-    //     try {
-    //         $file_name = time() . " - " . $request->hotel_photo->getClientOriginalName();
-    //         $file_name = str_replace(' ', '', $file_name);
-    //         $path_hotel_photo = asset("uploads/registrations/" . $file_name);
-    //         $request->hotel_photo->move(public_path('uploads/registrations/'), $file_name);
-
-    //         $file_name = time() . " - " . $request->hotel_logo->getClientOriginalName();
-    //         $file_name = str_replace(' ', '', $file_name);
-    //         $path_hotel_logo = asset("uploads/registrations/" . $file_name);
-    //         $request->hotel_logo->move(public_path('uploads/registrations/'), $file_name);
-
-    //         $hotel = Hotel::create([
-    //             'name' => $request->hotel_name,
-    //             'class' => $request->hotel_class,
-    //             'check_in' => $request->hotel_check_in,
-    //             'check_out' => $request->hotel_check_out,
-    //             'greeting' => $request->greeting,
-    //             'about' => $request->hotel_about,
-    //             'photo' => $path_hotel_photo,
-    //             'address' => $request->hotel_address,
-    //             'city' => $request->hotel_city,
-    //             'phone' => $request->hotel_phone,
-    //             'logo' => $path_hotel_logo,
-    //             'order_food_intro' => $request->order_food_intro,
-    //             'qr_code_payment' => $request->qr_code_payment,
-    //             'qr_code_wifi' => $request->qr_code_wifi,
-    //         ]);
-
-    //         $file_name = time() . " - " . $request->facility_image->getClientOriginalName();
-    //         $file_name = str_replace(' ', '', $file_name);
-    //         $path_facility_image = asset("uploads/registrations/" . $file_name);
-    //         $request->facility_image->move(public_path('uploads/registrations/'), $file_name);
-
-    //         $hotel_facility = HotelFacilities::create([
-    //             'hotel_id' => $hotel->id,
-    //             'name' => $request->facility_name,
-    //             'description' => $request->facility_description,
-    //             'image' => $path_facility_image
-    //         ]);
-
-    //         $file_name = time() . " - " . $request->room_image->getClientOriginalName();
-    //         $file_name = str_replace(' ', '', $file_name);
-    //         $path_room_image = asset("uploads/registrations/" . $file_name);
-    //         $request->room_image->move(public_path('uploads/registrations/'), $file_name);
-
-    //         $room = Room::create([
-    //             'hotel_id' => $hotel->id,
-    //             'type' => $request->room_type,
-    //             'facility' => $request->room_facility,
-    //             'description' => $request->room_description,
-    //             'image' => $path_room_image,
-    //             'television' => $request->room_television,
-    //         ]);
-
-    //         $file_name = time() . " - " . $request->room_service_image->getClientOriginalName();
-    //         $file_name = str_replace(' ', '', $file_name);
-    //         $path_room_service_image = asset("uploads/registrations/" . $file_name);
-    //         $request->room_service_image->move(public_path('uploads/registrations/'), $file_name);
-
-    //         $room_service = RoomService::create([
-    //             'room_id' => $room->id,
-    //             'name' => $request->room_service_name,
-    //             'image' => $path_room_service_image,
-    //         ]);
-
-    //         $file_name = time() . " - " . $request->menu_image->getClientOriginalName();
-    //         $file_name = str_replace(' ', '', $file_name);
-    //         $path_menu_image = asset("uploads/registrations/" . $file_name);
-    //         $request->menu_image->move(public_path('uploads/registrations/'), $file_name);
-
-    //         $menu = Menu::create([
-    //             'hotel_id' => $hotel->id,
-    //             'name' => $request->menu_name,
-    //             'description' => $request->menu_description,
-    //             'price' => $request->menu_price,
-    //             'rating' => $request->menu_rating,
-    //             'image' => $path_menu_image,
-    //         ]);
-
-    //         DB::commit();
-    //     } catch (\Throwable $th) {
-    //         DB::rollBack();
-    //         return response()->json([
-    //             'message' => 'failed to save data',
-    //             'errors' => $th->getMessage()
-    //         ]);
-    //     }
-
-    //     return response()->json([
-    //         'message' => 'Data saved successfully!',
-    //         'hotel' => $hotel,
-    //         'hotel_facility' => $hotel_facility,
-    //         'room' => $room,
-    //         'room_service' => $room_service,
-    //         'menu' => $menu,
-    //     ]);
-    // }
-
-    // public function show_content()
-    // {
-    //     return response()->json(Content::all());
-    // }
 }
