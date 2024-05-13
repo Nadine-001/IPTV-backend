392aa9342de40db8b9197380a595bf13137716c8
Nessa <na.nadine.001@gmail.com>
2024-05-13 20:07:25 +0700

    add TV channel list function

--- app/Http/Controllers/TelevisionController.php
+++ app/Http/Controllers/TelevisionController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Kreait\Firebase\Factory;
+use Kreait\Laravel\Firebase\Facades\Firebase;
 
 class TelevisionController extends Controller
 {
-    //
+    protected $auth, $rtdb;
+
+    public function __construct()
+    {
+        $this->auth = Firebase::auth();
+
+        $firebase = (new Factory)
+            ->withServiceAccount(base_path(env('FIREBASE_CREDENTIALS')));
+
+        $this->rtdb = $firebase->withDatabaseUri(env("FIREBASE_DATABASE_URL"))
+            ->createDatabase();
+    }
+
+    public function channel()
+    {
+        $streams = $this->rtdb->getReference('streams')->getValue();
+
+        $channel = [];
+        foreach ($streams as $key => $data) {
+            if (!is_null($data)) {
+                $channel[] = [
+                    'id' => $data['id'],
+                    'name' => $data['name'],
+                    'url' => $data['url'],
+                ];
+            }
+        }
+
+        return response()->json($channel);
+    }
 }
