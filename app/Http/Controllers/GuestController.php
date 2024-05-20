5f7ebbb60373c2f78358a13fbca2af0b3df65ced
Nessa <na.nadine.001@gmail.com>
2024-05-20 16:30:41 +0700

    add guest check-in and check-out time

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\GuestHistory;
 use App\Models\Hotel;
 use App\Models\Room;
 use App\Models\Television;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class GuestController extends Controller
@@ -59,19 +61,21 @@ class GuestController extends Controller
             'room_type' => 'required',
             'guest_name' => 'required',
             'guest_gender' => 'required',
+            'check_in' => 'required',
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
 
-        $room = Television::where('id', $request->room_number_id)->first();
-
         try {
+            $room = Television::where('id', $request->room_number_id)->first();
+
             $room->update([
                 'room_type' => $request->room_type,
                 'guest_name' => $request->guest_name,
                 'guest_gender' => $request->guest_gender,
+                'check_in' => $request->check_in,
             ]);
         } catch (\Throwable $th) {
             return response()->json([
@@ -96,12 +100,14 @@ class GuestController extends Controller
                 $room_number = $television->room_number;
                 $room_type = $television->room_type;
                 $guest_name = $television->guest_name;
+                $check_in = $television->check_in;
 
                 $guest_list[] = [
                     'room_number_id' => $room_number_id,
                     'room_number' => $room_number,
                     'room_type' => $room_type,
                     'guest_name' => $guest_name,
+                    'check_in' => $check_in,
                 ];
             }
         } catch (\Throwable $th) {
@@ -116,7 +122,8 @@ class GuestController extends Controller
         ]);
     }
 
-    public function guest_data($room_number_id) {
+    public function guest_data($room_number_id)
+    {
         $television = Television::where('id', $room_number_id)->first();
 
         try {
@@ -124,6 +131,7 @@ class GuestController extends Controller
             $room_type = $television->room_type;
             $guest_name = $television->guest_name;
             $guest_gender = $television->guest_gender;
+            $check_in = $television->check_in;
         } catch (\Throwable $th) {
             return response()->json([
                 'message' => 'failed to get guest data',
@@ -136,6 +144,7 @@ class GuestController extends Controller
             'room_type' => $room_type,
             'guest_name' => $guest_name,
             'guest_gender' => $guest_gender,
+            'check_in' => $check_in,
         ]);
     }
 
@@ -145,6 +154,7 @@ class GuestController extends Controller
             'room_type' => 'required',
             'guest_name' => 'required',
             'guest_gender' => 'required',
+            'check_in' => 'required',
         ]);
 
         if ($validator->fails()) {
@@ -158,6 +168,7 @@ class GuestController extends Controller
                 'room_type' => $request->room_type,
                 'guest_name' => $request->guest_name,
                 'guest_gender' => $request->guest_gender,
+                'check_in' => $request->check_in,
             ]);
         } catch (\Throwable $th) {
             return response()->json([
@@ -167,6 +178,49 @@ class GuestController extends Controller
         }
 
         return response()->json('guest data updated succesfully');
+    }
+
+    public function check_out(Request $request, $room_number_id)
+    {
+        $validator = Validator::make($request->all(), [
+            'check_out' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        try {
+            $television = Television::where('id', $room_number_id)->first();
+
+            DB::beginTransaction();
+
+            GuestHistory::create([
+                'hotel_id' => $television->hotel_id,
+                'room_number' => $television->room_number,
+                'guest_name' => $television->guest_name,
+                'check_in' => $television->check_in,
+                'check_out' => $request->check_out,
+            ]);
+
+            $television->update([
+                'guest_name' => null,
+                'guest_gender' => null,
+                'room_type' => null,
+                'check_in' => null,
+            ]);
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'failed to save check out time',
+                'errors' => $th->getMessage()
+            ], 400);
+        }
+
+        return response()->json([
+            'message' => 'success add data to history '
+        ]);
     }
 
     public function delete_guest($room_number_id)
