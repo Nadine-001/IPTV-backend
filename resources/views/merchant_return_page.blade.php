98268e6623c881eebd8d01d97d9374d831eff0d9
Nessa <na.nadine.001@gmail.com>
2024-07-08 22:29:59 +0700

    faspay return page view

---
+++ resources/views/merchant_return_page.blade.php
@@ -0,0 +1,25 @@
+<!-- resources/views/merchant_return_page.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Thanks</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1 class="display-3">{{ $message }}</h1>
+  <p class="lead"><strong>Please check your email</strong> for further detail transaction.</p>
+  <hr>
+  <p>
+    Having trouble? <a href="">Contact us</a>
+  </p>
+  <p class="lead">
+    <a class="btn btn-primary btn-sm" href="" role="button">Continue to homepage</a>
+  </p>
+</div>
+
+</body>
+</html>
